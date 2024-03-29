67b70616a52b84e21f06cd3316f132559ba38307
Esma Nazlı İşbilir <nazliisbilir.business@gmail.com>
2024-03-29 13:56:30 +0300

    player

--- src/components/Bottombar.jsx
+++ src/components/Bottombar.jsx
@@ -1,7 +1,9 @@
+import Player from "./Bottombar/Player"
+
 function Bottombar() {
     return (
       <div className="h-24 bg-footer border-t border-white border-opacity-5 fixed bottom-0 w-full right-0 left-0">
-          bottombar   
+          <Player />   
       </div>
     )
   }
