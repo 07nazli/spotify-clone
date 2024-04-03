a11b74857ca3b4ff1b85187af7a9b9aed72ada0a
Esma Nazlı İşbilir <nazliisbilir.business@gmail.com>
2024-04-03 22:17:11 +0300

    redux

--- src/components/Bottombar/Player.jsx
+++ src/components/Bottombar/Player.jsx
@@ -2,12 +2,23 @@ import { Icon } from "../../icons"
 import {useAudio} from 'react-use';
 import { secondsToTime } from "../../utils";
 import CustomRange from "../CustomRange";
+import { useMemo } from "react";
 
 function Player() {
 
     const [audio, state, controls, ref] = useAudio({
-        src: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3'
+        src: 'https://www.ssppdd.com/download_mp3?token=DMuqPWnOTkdjFksr_nU7dB2tj_BcS0IOHLD07VNzFp0&video_id=_lVWt2fDCas'
       });
+
+    const volumeIcon = useMemo(() => {
+        if(state.volume === 0 || state.muted)
+            return 'volumeMuted'
+        if(state.volume > 0 && state.volume < 0.33)
+            return 'volumeLow'
+        if(state.volume >= 0.33 && state.volume < 0.66)
+            return 'volumeNormal'
+        return 'volumeFull'
+    }, [state.volume, state.muted])
 
   return (
     <div className="flex px-4 justify-between items-center h-full">
@@ -63,16 +74,21 @@ function Player() {
         <button className="w-8 h-8 flex items-center justify-center text-white text-opacity-70 hover:text-opacity-100">
             <Icon size={16} name="device" />
         </button>
-        <button className="w-8 h-8 flex items-center justify-center text-white text-opacity-70 hover:text-opacity-100">
-            <Icon size={16} name="volumeFull" />
+        <button 
+            onClick={controls[state.muted ? 'unmute' : 'mute']}
+            className="w-8 h-8 flex items-center justify-center text-white text-opacity-70 hover:text-opacity-100">
+            <Icon size={16} name={volumeIcon} />
         </button>
         <div className="w-[5.813rem] max-w-full">
             <CustomRange
                 step={0.01}
                 min={0}
                 max={1}
-                value={state?.volume}
-                onChange={value => controls.volume(value)}
+                value={state.muted ? 0 : state?.volume}
+                onChange={value => {
+                    controls.unmute()
+                    controls.volume(value)
+                }}
             />
         </div>
         <button className="w-8 h-8 flex items-center justify-center text-white text-opacity-70 hover:text-opacity-100">
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -8,21 +8,24 @@ function Home() {
       title: "Soundtracks - OST",
       description: 'Original Soundtracks',
       image: 'https://cdn.dribbble.com/users/3756162/screenshots/15213760/media/ffeae34c81e5188c86b88f4aa1eee7e2.jpg?resize=1600x1200&vertical=center',
-      type: 'album'
+      type: 'album',
+      src: 'https://cdn.freesound.org/previews/730/730249_1015240-lq.mp3'
     },
     {
       id: 2,
       title: "Soundtracks - OST",
       description: 'Original Soundtracks',
       image: 'https://i.scdn.co/image/ab67706f00000002ca5a7517156021292e5663a6',
-      type: 'album'
+      type: 'album',
+      src: 'https://cdn.freesound.org/previews/709/709985_8432823-lq.mp3'
     },
     {
       id: 3,
       title: "Soundtracks - OST",
       description: 'Original Soundtracks',
       image: 'https://cdn.dribbble.com/users/3756162/screenshots/11531828/media/1321a8336b4ccd9b5c37f4652ac9bd13.jpg?resize=1600x1200&vertical=center',
-      type: 'artist'
+      type: 'artist',
+      src: 'https://cdn.freesound.org/previews/688/688767_10859468-lq.mp3'
     },
     {
       id: 4,
@@ -36,7 +39,8 @@ function Home() {
       title: "Soundtracks - OST",
       description: 'Original Soundtracks',
       image: 'https://cdn.dribbble.com/userupload/10214560/file/original-8c5cd8140d7503c6b166fe83e2dc2ecd.jpg?resize=1504x1128',
-      type: 'album'
+      type: 'album',
+      src: 'https://cdn.freesound.org/previews/560/560448_12295155-lq.mp3'
     }
   ]
 
